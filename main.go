5e74111ef51b3a1a23479f1e226ac95629a9463a
Dat Nguyen <dat.nguyen@datavtech.com>
2020-11-11 12:49:29 +0700

    add make file and docker file

--- main.go
+++ main.go
@@ -1,6 +1,3 @@
-// Copyright 2019 DataVTech authors. All rights reserved.
-// Author: bruce.do@datavtech.com
-
 package main
 
 import "datnguyen.stably.test/server"
--- stably_dashboard/src/api/prime.js
+++
@@ -1,5 +0,0 @@
-import api from '.'
-
-export function findPrime (number) {
-  return api.request('get', `stably/prime/highest/` + number)
-}
